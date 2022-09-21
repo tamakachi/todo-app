c5d2cd82f8cdbfb76b30f16e2771dcdc52996af5
Kent <kenttechnical@gmail.com>
2022-09-21 17:30:18 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import './bootstrap/bootstrap.css';
+import './App.css'
+import {useState} from 'react'
+
 
 function App() {
+
+ 
+const [list,setList] = useState(['Complete online Javascript course','Jog around the park 3x','10 Minutes Meditation','Read for 1 hour'],)
+
+
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col'></div>
+          <div className='col mainColumn'>
+            <h1>TODO</h1>
+            {list.map((item)=>(<div>{item}</div>))}
+          </div>
+          <div className='col'></div>
+          
+        </div>
+      </div>
     </div>
   );
 }
